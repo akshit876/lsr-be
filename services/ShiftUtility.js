83a05d107f59213c5aaa17d5217315ecdbc15b98
akshit <s.akshit1267@gmail.com>
2024-10-26 23:29:33 +0530

    shift updates

--- services/ShiftUtility.js
+++ services/ShiftUtility.js
@@ -161,7 +161,7 @@ async function run() {
   // console.log(`Shift A End Time: ${shiftAEndTime}`);
 }
 
-// Run the example
-run().catch((error) => {
-  console.error("Error running the shift utility:", error);
-});
+// // Run the example
+// run().catch((error) => {
+//   console.error("Error running the shift utility:", error);
+// });
