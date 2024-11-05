9b4be0ec5dd1d9f420948cf2a93ed492122edddc
akshit <s.akshit1267@gmail.com>
2024-11-05 14:35:37 +0530

    update reset detection time

--- services/resetMonitor.js
+++ services/resetMonitor.js
@@ -8,12 +8,12 @@ async function monitorResetSignal() {
         logger.info('Reset monitor connected to Modbus');
 
         while (true) {
-            const resetSignal = await readBit(1600, 0);
-            if (resetSignal) {
-                logger.info('Reset signal detected (1600.0 = 1)');
-                parentPort.postMessage('reset');
-            }
-            await new Promise(resolve => setTimeout(resolve, 100)); // Check every 100ms
+          const resetSignal = await readBit(1600, 0);
+          if (resetSignal) {
+            logger.info("Reset signal detected (1600.0 = 1)");
+            parentPort.postMessage("reset");
+          }
+          await new Promise((resolve) => setTimeout(resolve, 10)); // Check every 100ms
         }
     } catch (error) {
         console.log({ error });
